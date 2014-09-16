ec72d3dc83d10f3940de6cf104bba9e3c53dcf87
Michael Barreiro <Mikeb_barreiro@yahoo.com>
2014-09-16 14:50:04 -0500

    Goals

--- client/views/goals/goal.js
+++ client/views/goals/goal.js
@@ -3,8 +3,20 @@
 
   angular.module('mean-template')
   .controller('GoalCtrl', ['$scope', 'Goal', function($scope, Goal){
+    $scope.goals = [];
+    $scope.calGoal = 0;
 
+    $scope.create = function(){
+      Goal.create($scope.calGoal).then(function(response){
+        $scope.goals.push(response.data.goal);
+      });
+    };
 
+    $scope.all = function(){
+      Goal.all().then(function(response){
+        $scope.goals = response.data.goals;
+      });
+    };
   }]);
 })();
 
